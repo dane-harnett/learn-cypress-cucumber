305eb0a9194cd30ca9808da3173ba98a7f40e37e
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2021-08-01 14:14:26 +1000

    Attach screenshots to reports

--- todomvc-typescript-react/cypress/support/index.js
+++ todomvc-typescript-react/cypress/support/index.js
@@ -12,9 +12,10 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
+import "cypress-cucumber-attach-screenshots-to-failed-steps";
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
